dcee1d25b36642a5dfb58b81e4e504ba5571dfad
thomascarre <carrethomas7@gmail.com>
2022-07-16 18:43:48 +0200

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_16_162752) do
+ActiveRecord::Schema.define(version: 2022_07_16_162132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "desks", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-  end
-
-  create_table "requests", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "desk_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["desk_id"], name: "index_requests_on_desk_id"
-    t.index ["user_id"], name: "index_requests_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -45,6 +30,4 @@ ActiveRecord::Schema.define(version: 2022_07_16_162752) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "requests", "desks"
-  add_foreign_key "requests", "users"
 end
